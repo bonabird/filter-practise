450a89f2ede53c85776c50e34677048bf3b12dc7
bonabird <34163166+bonabird@users.noreply.github.com>
2022-07-02 12:21:33 +0200

    Stremlined code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Data from "./data";
 import Card from "./card";
 import Buttons from "./buttons";
